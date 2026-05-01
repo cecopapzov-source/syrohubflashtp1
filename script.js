d68426a58766e72a2f1728738b22e4416cb186da
cecopapzov-source <cecopapzov@gmail.com>
2026-05-01 19:37:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    document.body.innerHTML = '<div style="display:flex; justify-content:center; align-items:center; height:100vh; background:#000; color:red; font-size:30px; font-family:sans-serif; font-weight:bold;">THIS CANT DISPLAYED HERE!</div>';
+    console.clear();
+};
